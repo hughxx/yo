39489d77bcc4c7bb27723fa8a4d0ed950b368e8a
hugh <1143239548@qq.com>
2026-05-15 16:32:50 +0800

    fix: 私聊跳过自己发的消息，避免误触发回复

--- pyqt_client/modules/welink/autoreply_monitor.py
+++ pyqt_client/modules/welink/autoreply_monitor.py
@@ -201,6 +201,10 @@ class AutoReplyMonitor(QThread):
             return
         self._last_ids[account] = lid
 
+        if lm.get('sender') != account:
+            self._log(f'[私聊][{account}] 跳过（自己发的消息）')
+            return
+
         content = _norm(lm.get('content', '')).strip()
         self._log(f'[私聊][{account}] 新消息: {content[:60]}')
         if not content:
